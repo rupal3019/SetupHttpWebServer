e6cc345f2ce27a400382d4bbf71911c7692ecf60
Yash Agarwal <agarwalyash929@gmail.com>
2023-11-13 15:02:41 +0530

    added more endpoints

--- index.py
+++ index.py
@@ -6,9 +6,30 @@ class basicRequesthandler(tornado.web.RequestHandler):
     def get(self):
         self.write("hey!! its me")
 
+class skillsRequesthandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if (num.isdigit()):
+            r = "odd" if int(num)%2!=0 else "even"
+            self.write(f"the integer {num} is {r}")
+        else:
+            self.write(f"the num {num} is not a valid integer")
+ 
+
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+    def get(self,studentName,courseID):
+        self.write(f"welcome {studentName}, the course you are viewing is {courseID}") 
+
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/",basicRequesthandler)
+        (r"/",basicRequesthandler),
+        (r"/skills",skillsRequesthandler),
+        (r"/checkNumber",queryParamRequestHandler),
+        (r"/students/([a-z]+)/([0-9]+)",resourceParamRequestHandler)
     ])
 
     port = 8082
