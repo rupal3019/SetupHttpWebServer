1263bf678c08e5a411abfa3fbc10400483ae1567
Yash Agarwal <agarwalyash929@gmail.com>
2023-12-01 17:57:50 +0530

    Added list and add skill things

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import tornado.web
 import tornado.ioloop
-
+import json
 
 class basicRequesthandler(tornado.web.RequestHandler):
     def get(self):
@@ -24,12 +24,28 @@ class resourceParamRequestHandler(tornado.web.RequestHandler):
     def get(self,studentName,courseID):
         self.write(f"welcome {studentName}, the course you are viewing is {courseID}") 
 
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        fl = open('names.txt','r')
+        skills = fl.read().splitlines()
+        fl.close()
+        self.write(json.dumps(skills))
+
+    def post(self):
+        skill = self.get_argument("skill")
+        fl = open('names.txt','a')
+        fl.write(f"{skill}\n")
+        fl.close()
+        self.write(json.dumps({"message":"skill added successfully"}))
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/",basicRequesthandler),
         (r"/skills",skillsRequesthandler),
         (r"/checkNumber",queryParamRequestHandler),
-        (r"/students/([a-z]+)/([0-9]+)",resourceParamRequestHandler)
+        (r"/students/([a-z]+)/([0-9]+)",resourceParamRequestHandler),
+        (r"/list",listRequestHandler)
+
     ])
 
     port = 8082
